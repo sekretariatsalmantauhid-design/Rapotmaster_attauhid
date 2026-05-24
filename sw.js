b1a8cf761da85ed699df9fccc565f21138efc2a8
SalmanDesaign-er <sekretariat.salmantauhid@gmail.com>
2026-05-24 18:54:29 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+// UBAH ANGKA INI SETIAP KALI UPDATE index.html (Misal: v2, v3, v4)
+const CACHE_NAME = 'erapot-admin-v1.0'; 
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest_admin.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+];
+
+// Install Service Worker & Paksa Langsung Aktif
+self.addEventListener('install', event => {
+  self.skipWaiting(); // <--- Trik Rahasia 1: Paksa satpam ganti shift!
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aplikasi Master Admin versi baru siap!');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Strategi: Ambil dari Internet dulu (Network First)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+// Bersihkan Cache versi lama saat update
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Menghapus cache Admin jadul:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim(); // <--- Trik Rahasia 2: Ambil alih layar HP user!
+    })
+  );
+});
