ebff9226871d76eb97596b0612f4ab09b5bd5f28
SalmanDesaign-er <sekretariat.salmantauhid@gmail.com>
2026-06-12 04:19:24 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UBAH ANGKA INI SETIAP KALI UPDATE index.html (Misal: v2, v3, v4)
-const CACHE_NAME = 'erapot-admin-v1.1'; 
+const CACHE_NAME = 'erapot-admin-v1.2'; 
 
 const urlsToCache = [
   './',
